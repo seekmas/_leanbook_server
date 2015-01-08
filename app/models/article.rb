cdde8d90035a8469936622f4c502dfc46ac67d0e
seekmas <seekmas@msn.cn>
2015-01-08 11:47:55 +0800

    fixs_3

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
-  belongs_to :category , -> { select(:id , :subject, :short, :photo_thumb) }, :foreign_key => :category_id
+  belongs_to :category , :foreign_key => :category_id
 
   has_many :tags
   has_many :comments , -> { order(id: :desc)}
